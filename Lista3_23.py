4eb57b2940e4bb519742b0fd83af538d14b66647
GuilhermeHorn2 <74984552+GuilhermeHorn2@users.noreply.github.com>
2021-04-09 00:09:33 -0300

    Update Lista3_23.py

--- Lista3_23.py
+++ Lista3_23.py
@@ -1,12 +1,16 @@
-numero = int(input("\nDigite um número: "))
-lista = []
-divisoes = 0
+n = int(input(':'))
 
-for i in range(numero + 1):
-    if i % 2 == 1 and i != 2:
-        lista.append(i)
-        divisoes += 1
+div = 0
+
+r = []
+for i in range(n +1):
+    if i % 2 == 1 and i != 2 and i !=1:
+        r.append(i)
+        div += 1
+    if i == 2:
+        r.append(i)
     else:
-        divisoes += 1
-print("Números primos: ", lista)
-print("Número de divisões", divisoes)
+        div += 1
+print(r)
+print(div)
+
